03b609b04de8c4773821619c0fe2ea178927b835
hotzhangle <494482081@qq.com>
2017-03-19 19:23:06 +0800

    Update pasteVisualRange.vb
    1、能够防止用户选择了过大的区域。
    2、能够防止用户选择了隐藏单元格区域。
    3、能够有效解决源区域和目标区域中的隐藏列的问题。
    4、解决了利用窗体进行弹出窗口1秒提示功能。
    5、在区域选择的时候进行了防呆处理。

--- pasteVisualRange.vb
+++ pasteVisualRange.vb
@@ -5,13 +5,27 @@ Private Sub pasteVisualRange()
     Dim counter As Integer, index As Integer, rowOffset As Integer, columnOffset As Integer, hiddenRowCounter As Integer, hiddenColumnCounter As Integer
     Dim sourceVisualRowCounter%, sourceVisualColumnCounter%, distVisualRowCounter%, distVisualColumnCounter%, StartRow%, startColumn%, rowCounter%
     Dim rowColumnStr As String, str As String, sheetName As String, workbookName As String
-    Dim d As Object
+    Dim d As Object, WsShell As Object
     Dim arr As Variant
     
+    Set WshShell = CreateObject("Wscript.Shell")
+    
     '##################################选择单元格区域########################
+rechoiceSource:
     message1 = "请选择你要粘贴的数据源的区域："
     Title1 = "数据源区域"
     Set sourceRange = Application.InputBox(prompt:=message1, Title:=Title1, Type:=8) 'inputbox需要接收返回值
+    
+    If sourceRange.Rows.Count = 0 Or sourceRange.Columns.Count = 0 Then
+        GoTo rechoiceSource
+    End If
+    
+    str = "选择区域共" & vbCtLf & sourceRange.Rows.Count & "行" & vbCtLf & sourceRange.Columns.Count & "列" & vbCtLf & "确认选择吗？"
+    If sourceRange.Rows.Count * sourceRange.Columns.Count > 100000 Then
+        If vbCancel = (MsgBox(str, vbOKCancel + vbDefaultButton2, "确认选择")) Then '之前从网上抄的messageButtonStyle.vbOKCancel,这种会报错
+            GoTo rechoiceSource
+        End If
+    End If
     
     Set d = CreateObject("scripting.dictionary") '创建字典
     
@@ -37,7 +51,10 @@ rechoice:
     sheetName = distRange.Parent.Name
     workbookName = distRange.Parent.Parent.Name
     Workbooks(workbookName).Sheets(sheetName).Activate
-    MsgBox "当前活动工作簿为：" & vbCrLf & ActiveWorkbook.Name & vbCrLf & "活动工作表为：" & ActiveSheet.Name '换行显示
+    'MsgBox "当前活动工作簿为：" & vbCrLf & ActiveWorkbook.Name & vbCrLf & "活动工作表为：" & ActiveSheet.Name '换行显示
+    '这里时间是int类型的，所以不能少于1秒
+    '这个定时器有可能会失效，失效就手动点击或者重启Application
+    WshShell.Popup "当前活动工作簿为：" & vbCrLf & ActiveWorkbook.Name & vbCrLf & "活动工作表为：" & ActiveSheet.Name, 1, "1秒后关闭！"
     ''##################激活粘贴目标工作表##########################
     
     '##################################把数据源存放在字典当中########################
@@ -64,7 +81,8 @@ rechoice:
         End If
     Next
     
-    MsgBox "当前选择区域的可见行列数为" & sourceVisualRowCounter & "行" & sourceVisualColumnCounter - 1 & "列"
+    'MsgBox "当前选择区域的可见行列数为" & sourceVisualRowCounter & "行" & sourceVisualColumnCounter - 1 & "列"
+    WshShell.Popup "当前选择区域的可见行列数为" & sourceVisualRowCounter & "行" & sourceVisualColumnCounter - 1 & "列", 1, "1秒后关闭！"
     '##################################把数据源存放在字典当中########################
     
     '##################################把字典中的值转存到目标区域中########################
@@ -121,6 +139,14 @@ nextColumn:
     Loop
     '##################################把字典中的值转存到目标区域中########################
     Application.ScreenUpdating = True
+    WshShell.Popup "复制完成", 1, "1秒后关闭！"
+    Set WshShell = Nothing
+    Set d = Nothing
     
+End Sub
+
+Public Sub 录入对话()
+    '过程,"弹出对话","对话框标题",图标类型,默认参数,N秒后自动关闭
+    MsgBoxTimeOut 0, "录入完毕!!", "提示", 64, 0, 1500
 End Sub
 
