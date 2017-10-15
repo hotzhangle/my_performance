387566b41f2ebc30185bb2f467095633b4897471
hotzhangle <494482081@qq.com>
2017-10-15 11:30:31 +0800

    Update aboutSignApkScripts.sh

--- aboutSignApkScripts.sh
+++ aboutSignApkScripts.sh
@@ -11,19 +11,19 @@ adb reboot bootloader
 fastboot flash boot boot.img && fastboot reboot
 # adb reboot
 function getCurrentWindowClass(){
-#get current window class name
-current_window_class=`adb shell "dumpsys window | grep -i mcurrent | tac | head -1" | sed -e 's#^.*\(com.*\)\/\(.*\)}$#\2#' 2>/dev/null`
-echo $current_window_class
+	#get current window class name
+	current_window_class=`adb shell "dumpsys window | grep -i mcurrent | tac | head -1" | sed -e 's#^.*\(com.*\)\/\(.*\)}$#\2#' 2>/dev/null`
+	echo $current_window_class
 }
 adb "wait-for-devices"
 echo "wait-for-device" 
 
 while [[ $(getCurrentWindowClass) != "com.android.launcher3.Launcher" ]]; do
-#getCurrentWindowClass
-sleep 10
-adb shell "input keyevent 82 2>/dev/null"
-sleep 2
-adb shell "input keyevent 82 2>/dev/null"
+	#getCurrentWindowClass
+	sleep 10
+	adb shell "input keyevent 82 2>/dev/null"
+	sleep 2
+	adb shell "input keyevent 82 2>/dev/null"
 done
 adb shell "am start -n com.android.huaqin.factory/com.android.huaqin.factory.ControlCenterActivity" 2>/dev/null
 sleep 2
